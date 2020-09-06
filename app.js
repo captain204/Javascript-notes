a1941381f9a4c30ac6a0a7ae962aa9208ee77c91
ramkodgreat <nurudeenakindele8@gmail.com>
2020-09-06 17:23:52 +0100

    undefined

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+b();
+
+console.log(a);
+
+
 var a = "Hello World!";
 
 function b(){
 
-    
+    console.log('Called b!');
 }
+
---
+++ undefined.js
@@ -0,0 +1,13 @@
+
+var a;
+
+console.log(a)
+
+if(a === undefined){
+
+    console.log("The variable is undefined");
+}else
+{
+
+    console.log("The variable is defined");
+}
