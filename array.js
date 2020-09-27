ac28ccb37f25bbc38775d6202f738cfcf09dfaf5
ramkodgreat <nurudeenakindele8@gmail.com>
2020-09-27 13:12:03 +0100

    Javascript arrays

---
+++ array.js
@@ -0,0 +1,20 @@
+var arr = [
+    1,
+    false,
+    {
+        name:'Tony Picasso',
+        address:'Unilag admin block',
+    },
+    function(name){
+        var greeting = "hello ";
+        console.log(greeting  +  name);
+    },
+    "Hello"
+];
+console.log(arr);
+
+arr[3](arr[2].name);
+arr[3]("Mike");
+
+
+
--- object_this.js
+++ object_this.js
@@ -1,1 +1,28 @@
-console.log(this);
+function a(){
+    console.log(this);
+    this.newVariable = "Hello";
+}
+
+var b = function(){
+    console.log(this);
+}
+a();
+b();
+console.log(newVariable);
+
+var c = {
+    name:'The  c object',
+    log:function(){
+        var self = this;
+        self.name = "Updated c object";
+        console.log(self);
+
+        var setname = function(newname){
+            self.name = newname;
+        }
+        setname('Update the c object again make we see');
+        console.log(self);
+    }
+}
+
+c.log();
